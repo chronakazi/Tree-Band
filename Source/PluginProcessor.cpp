afd8ded33d5854be30dd523606141f7e7a81ba81
gmb <gb1024@live.com>
2024-09-23 17:31:52 -0400

    linkwitz-riley filter xover. disabled compressor temporarily for filter testing

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -43,6 +43,10 @@ TreeBandAudioProcessor::TreeBandAudioProcessor()
         jassert(param != nullptr);
     };
     
+    LP.setType(juce::dsp::LinkwitzRileyFilterType::lowpass);
+    HP.setType(juce::dsp::LinkwitzRileyFilterType::highpass);
+    
+    floatHelper(lowCrossover, Names::Low_Mid_Crossover_Freq);
     floatHelper(compressor.threshold, Names::Threshold_Low_Band);
     floatHelper(compressor.attack, Names::Attack_Low_Band);
     floatHelper(compressor.release, Names::Release_Low_Band);
@@ -130,6 +134,14 @@ void TreeBandAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlo
     
     compressor.prepare(spec);
     
+    LP.prepare(spec);
+    HP.prepare(spec);
+    
+    for (auto& buffer : filterBuffers)
+    {
+        buffer.setSize(spec.numChannels, samplesPerBlock);
+    }
+    
 }
 
 void TreeBandAudioProcessor::releaseResources()
@@ -179,8 +191,42 @@ void TreeBandAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juc
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
     
-    compressor.updateCompressorSettings();
-    compressor.process(buffer);
+//    compressor.updateCompressorSettings();
+//    compressor.process(buffer);
+    
+    for (auto& fb : filterBuffers)
+    {
+        fb = buffer;
+    }
+    
+    auto cutoff = lowCrossover->get();
+    LP.setCutoffFrequency(cutoff);
+    HP.setCutoffFrequency(cutoff);
+    
+    auto fb0Block = juce::dsp::AudioBlock<float>(filterBuffers[0]);
+    auto fb1Block = juce::dsp::AudioBlock<float>(filterBuffers[1]);
+    
+    auto fb0Ctx = juce::dsp::ProcessContextReplacing<float>(fb0Block);
+    auto fb1Ctx = juce::dsp::ProcessContextReplacing<float>(fb1Block);
+    
+    LP.process(fb0Ctx);
+    HP.process(fb1Ctx);
+    
+    auto numSamples = buffer.getNumSamples();
+    auto numChannels = buffer.getNumChannels();
+    
+    buffer.clear();
+    
+    auto addFilterBand = [nc = numChannels, ns = numSamples](auto& inputBuffer, const auto& source)
+    {
+        for (auto i = 0; i < nc; ++i)
+        {
+            inputBuffer.addFrom(i, 0, source, i, 0, ns);
+        }
+    };
+    
+    addFilterBand(buffer, filterBuffers[0]);
+    addFilterBand(buffer, filterBuffers[1]);
     
 }
 
@@ -259,6 +305,11 @@ juce::AudioProcessorValueTreeState::ParameterLayout TreeBandAudioProcessor::crea
     layout.add(std::make_unique<AudioParameterBool>(params.at(Names::Bypassed_Low_Band),
                                                      params.at(Names::Bypassed_Low_Band),
                                                      false));
+    
+    layout.add(std::make_unique<AudioParameterFloat>(params.at(Names::Low_Mid_Crossover_Freq),
+                                                      params.at(Names::Low_Mid_Crossover_Freq),
+                                                      NormalisableRange<float>(20, 20000, 1, 1),
+                                                      500));
     
     return layout;
 }
