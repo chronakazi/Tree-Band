660e44011bb2f97ccc4ac81757c25f7321f418e7
gmb <gb1024@live.com>
2024-09-30 13:24:21 -0400

    attached global params to global controls

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -24,6 +24,81 @@ enum ColorPalette
     Pop = 0xffad7976
 };
 
+struct LookAndFeel : juce::LookAndFeel_V4
+{
+    void drawRotarySlider(juce::Graphics&,
+                          int x, int y, int width, int height,
+                          float sliderPosProportional,
+                          float rotaryStartAngle,
+                          float rotaryEndAngle,
+                          juce::Slider&) override;
+    
+    void drawToggleButton (juce::Graphics & g,
+                           juce::ToggleButton & button,
+                           bool shouldDrawButtonAsHighlighted,
+                           bool shouldDrawButtonAsDown) override;
+};
+
+struct RotarySliderWithLabels : juce::Slider
+{
+    RotarySliderWithLabels(juce::RangedAudioParameter& rap, const juce::String& unitSuffix) :
+    juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+                 juce::Slider::TextEntryBoxPosition::NoTextBox),
+    param(&rap),
+    suffix(unitSuffix)
+    {
+        setLookAndFeel(&lnf);
+    }
+    
+    ~RotarySliderWithLabels()
+    {
+        setLookAndFeel(nullptr);
+    }
+    
+    struct LabelPos
+    {
+        float pos;
+        juce::String label;
+    };
+    
+    juce::Array<LabelPos> labels;
+    
+    void paint(juce::Graphics& g) override;
+    juce::Rectangle<int> getSliderBounds() const;
+    int getTextHeight() const {return 14;}
+    juce::String getDisplayString() const;
+    
+private:
+    LookAndFeel lnf;
+    juce::RangedAudioParameter* param;
+    juce::String suffix;
+};
+
+struct PowerButton : juce::ToggleButton {};
+struct AnalyzerButton : juce::ToggleButton
+{
+    void resized() override
+        {
+            auto bounds = getLocalBounds();
+            auto insetRect = bounds.reduced(4);
+            
+            randomPath.clear();
+            
+            juce::Random r;
+            
+            randomPath.startNewSubPath(insetRect.getX(),
+                                       insetRect.getY() + insetRect.getHeight() * r.nextFloat());
+            
+            for( auto x = insetRect.getX() + 1; x < insetRect.getRight(); x += 4 )
+            {
+                randomPath.lineTo(x,
+                                  insetRect.getY() + insetRect.getHeight() * r.nextFloat());
+            }
+        }
+        
+        juce::Path randomPath;
+};
+
 struct Placeholder : juce::Component
 {
     Placeholder();
@@ -34,6 +109,48 @@ struct Placeholder : juce::Component
     }
     
     juce::Colour customColor;
+};
+
+struct RotarySlider : juce::Slider
+{
+    RotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+                                  juce::Slider::TextEntryBoxPosition::NoTextBox)
+    {}
+};
+
+template<
+    typename Attachment,
+    typename APVTS,
+    typename Params,
+    typename ParamName,
+    typename SliderType
+    >
+void makeAttachment(std::unique_ptr<Attachment>& attachment,
+                    APVTS& apvts,
+                    const Params& params,
+                    const ParamName& name,
+                    SliderType& slider)
+{
+    attachment = std::make_unique<Attachment>(apvts,
+                                              params.at(name),
+                                              slider);
+}
+
+struct GlobalControls : juce::Component
+{
+    GlobalControls(juce::AudioProcessorValueTreeState& apvts);
+    
+    void paint(juce::Graphics& g) override;
+    
+    void resized() override;
+private:
+    RotarySlider inGainSlider, lowMidXoverSlider, midHighXoverSlider, outGainSlider;
+    
+    using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+    std::unique_ptr<Attachment> inGainSliderAttachment,
+                                 lowMidXoverSliderAttachment,
+                                 midHighXoverSliderAttachment,
+                                 outGainSliderAttachment;
 };
 
 /**
@@ -57,7 +174,8 @@ private:
     std::unique_ptr<melatonin::Inspector> inspector;
     juce::TextButton inspectButton { "Inspect the UI" };
     
-    Placeholder controlBar, analyzer, globalControls, bandControls;
+    Placeholder controlBar, analyzer, /*globalControls,*/ bandControls;
+    GlobalControls globalControls {audioProcessor.apvts};
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (TreeBandAudioProcessorEditor)
 };
